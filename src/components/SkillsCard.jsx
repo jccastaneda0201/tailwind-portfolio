592718698481d9371f94f6ee8ab92015e00c0de0
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-08-05 17:41:05 +0200

    updated skills components and styles

---
+++ src/components/SkillsCard.jsx
@@ -0,0 +1,11 @@
+const SkillsCard = ({ icon, title, text }) => {
+  return (
+    <article>
+      <span>{icon}</span>
+      <h4 className="mt-6 font-bold">{title}</h4>
+      <p className="mt-2 text-gray-600">{text}</p>
+    </article>
+  );
+};
+
+export default SkillsCard;
